0a4a1e69aee118cdc291adfb58fd107b69d548d8
Lorenzo Salani <46.lore.46@gmail.com>
2019-09-24 23:35:36 +0200

    first commit created project

---
+++ src/main/java/com/yard/biro/BiroApplication.java
@@ -0,0 +1,13 @@
+package com.yard.biro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiroApplication.class, args);
+	}
+
+}
