a1fa8223a89494d9d52896fd3d6008532335285c
Lorenzo Salani <46.lore.46@gmail.com>
2019-10-03 20:04:20 +0200

    changed path

--- src/main/java/com/yard/biro/UserRestController.java
+++ src/main/java/com/yard/biro/UserRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api")
 public class UserRestController{
 
     @Autowired
