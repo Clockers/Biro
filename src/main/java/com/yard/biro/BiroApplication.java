afc9d688303ef741fffc5a507773683ad57be001
Lorenzo Salani <46.lore.46@gmail.com>
2019-10-03 20:51:53 +0200

    added jwt.secret and une default user for tests

--- src/main/java/com/yard/biro/BiroApplication.java
+++ src/main/java/com/yard/biro/BiroApplication.java
@@ -1,13 +1,22 @@
 package com.yard.biro;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BiroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BiroApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demoData(UserRepository repo) {
+		return args -> {
+			repo.save(new User(null,"username","$2a$12$mT1FXVKWodQBn8RHMpYrOOilaW8NiodDd8k1anfX7WQGSqGMgOj8C"));
+		};
 	}
 
 }
