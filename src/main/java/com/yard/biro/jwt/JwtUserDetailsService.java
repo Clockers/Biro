9fcd5f8e1576e82c6d69c90284edb3c17aed6ed1
Lorenzo Salani <46.lore.46@gmail.com>
2019-10-03 20:53:00 +0200

    added jwtService

---
+++ src/main/java/com/yard/biro/jwt/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.yard.biro.jwt;
+
+import com.yard.biro.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.yard.biro.User user = userRepository.findByUsername(username);
+        return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
